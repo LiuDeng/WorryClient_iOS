0423cfbb2f83b25f7c626b4e2badf490aa340d6b
Shaowu Cai <jiandandream@foxmail.com>
2015-05-10 10:26:06 +0800

    add SendBlessingController

---
+++ WorryClient/Function/Timeline/Controller/Foundation/SendBlessingController.h
@@ -0,0 +1,13 @@
+//
+//  SendBlessingController.h
+//  WorryClient
+//
+//  Created by 蔡少武 on 15/5/10.
+//  Copyright (c) 2015年 jiandan. All rights reserved.
+//
+
+#import "DefaultViewController.h"
+
+@interface SendBlessingController : DefaultViewController
+
+@end
---
+++ WorryClient/Function/Timeline/Controller/Foundation/SendBlessingController.m
@@ -0,0 +1,37 @@
+//
+//  SendBlessingController.m
+//  WorryClient
+//
+//  Created by 蔡少武 on 15/5/10.
+//  Copyright (c) 2015年 jiandan. All rights reserved.
+//
+
+#import "SendBlessingController.h"
+
+@interface SendBlessingController ()
+
+@end
+
+@implementation SendBlessingController
+
+#pragma mark - Default methods
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)loadView
+{
+    [super loadView];
+    self.title = @"祝福";
+}
+
+#pragma mark - Private methods
+
+@end
