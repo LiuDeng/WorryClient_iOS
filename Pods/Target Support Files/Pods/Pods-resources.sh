3248baf75f7784e360d26e0d3594fe80bea66ff0
Shaowu Cai <jiandandream@foxmail.com>
2015-06-06 20:03:39 +0800

    update

--- Pods/Target Support Files/Pods/Pods-resources.sh
+++ Pods/Target Support Files/Pods/Pods-resources.sh
@@ -47,9 +47,10 @@ install_resource()
       ;;
   esac
 }
-          install_resource "AVOSCloud/iOS/release-v3.1.1.1/AVOSCloud.framework/AVOSCloud.bundle"
+          install_resource "AVOSCloud/iOS/release-v3.1.2.2/AVOSCloud.framework/AVOSCloud.bundle"
                     install_resource "CLImageEditor/CLImageEditor/CLImageEditor.bundle"
                     install_resource "MJRefresh/MJRefreshExample/MJRefreshExample/MJRefresh/MJRefresh.bundle"
+                    install_resource "Vertigo/Vertigo/TGRImageViewController.xib"
           
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 if [[ "${ACTION}" == "install" ]]; then
